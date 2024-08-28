74396c7ac67d302571353e89c9f53091812e25eb
Faith Kariuki <tcwamboi@gmail.com>
2024-08-28 11:17:30 +0300

    Complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])
+        return
+    a, b = 0, 1
+    sequence = []
+    for _ in range(length):
+        sequence.append(a)
+        a, b = b, a + b
+    print(sequence)
+    
